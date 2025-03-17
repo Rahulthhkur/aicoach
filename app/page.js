b9e65b58860db6237693702c83dc14eaba362f76
Rahulthhkur <rthakur1233@gmail.com>
2025-03-17 17:02:20 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return <div> Hello World
+    <br/>
+    <Button>Heyyy</Button>
+    
+     </div> ;
+}
