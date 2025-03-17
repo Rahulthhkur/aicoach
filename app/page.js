016777c0ead1d9577d259703ae14138f34515d37
Rahulthhkur <rthakur1233@gmail.com>
2025-03-17 18:28:30 +0530

    clerk added

--- app/page.js
+++ app/page.js
@@ -2,9 +2,6 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div> Hello World
-    <br/>
-    <Button>Heyyy</Button>
-    
+  return <div> 
      </div> ;
 }
