c5d602a4bea962c5982342396922aa84af730cc1
Rahulthhkur <rthakur1233@gmail.com>
2025-03-21 14:38:32 +0530

    industry insight and interviee page made

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,15 +1,20 @@
+import { getIndustryInsights } from "@/actions/dashboard";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
+import DashboardView from "./_components/dashboard-view";
 
 const IndustryInsightsPage = async () => {
   try {
     const { isOnboarded } = await getUserOnboardingStatus();
+    const insights = await getIndustryInsights();
 
     if (!isOnboarded) {
       return redirect('/onboarding');
     }
 
-    return <div>IndustryInsightsPage</div>;
+    return <div className="container mx-auto">
+      <DashboardView insights={insights}/>
+    </div>;
   } catch (error) {
     console.error("Error fetching onboarding status:", error.message);
     return redirect('/onboarding'); // Redirect if error occurs
